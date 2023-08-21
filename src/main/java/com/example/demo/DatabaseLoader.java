6ffbea1e6f9e05fa187ac8b6bffe97d171e42705
andyHA18 <103910962+andyHA18@users.noreply.github.com>
2023-08-21 17:23:49 +0000

    UPDATE

--- src/main/java/com/example/demo/DatabaseLoader.java
+++ src/main/java/com/example/demo/DatabaseLoader.java
@@ -28,27 +28,27 @@ public class DatabaseLoader implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
 
-        Producto pro1 = new Producto("CocaCola", 10);
-        Producto pro2 = new Producto("Bimbo", 2.5);
-        Producto pro3 = new Producto("Pepsi", 3);
+        Producto pro1 = new Producto("Bembos", 20);
+        Producto pro2 = new Producto("Galletas", 2.5);
+        Producto pro3 = new Producto("Gaseosa", 5);
 
         this.repositoryP.save(pro1);
         this.repositoryP.save(pro2);
         this.repositoryP.save(pro3);
 
 
-        VentaLugar ven1 = new VentaLugar("Arequipa");
-        VentaLugar ven2 = new VentaLugar("Lima");
-        VentaLugar ven3 = new VentaLugar("Piura");
+        VentaLugar ven1 = new VentaLugar("LIMA");
+        VentaLugar ven2 = new VentaLugar("ANCHON");
+        VentaLugar ven3 = new VentaLugar("HUANUCO");
 
         this.repositoryV.save(ven1);
         this.repositoryV.save(ven2);
         this.repositoryV.save(ven3);
 
 
-        VentaDetalle det1 = new VentaDetalle("A001",ven1, pro1, 2);
-        VentaDetalle det2 = new VentaDetalle("A002",ven2, pro2, 10);
-        VentaDetalle det3 = new VentaDetalle("A003",ven3, pro3, 5);
+        VentaDetalle det1 = new VentaDetalle("B001",ven1, pro1, 4);
+        VentaDetalle det2 = new VentaDetalle("B002",ven2, pro2, 12);
+        VentaDetalle det3 = new VentaDetalle("B003",ven3, pro3, 8);
 
         this.repositoryD.save(det1);
         this.repositoryD.save(det2);
